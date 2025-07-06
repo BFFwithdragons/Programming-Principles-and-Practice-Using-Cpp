928f4e41d96528c444b03042fb2044a26084c3e3
BFFwithdragons <elijahjchung@gmail.com>
2025-07-06 10:03:05 -0700

    Chapter 7 reading done

---
+++ Programming-code/Chapter06/twentyfourcalc.cpp
@@ -0,0 +1,17 @@
+#include "std_lib_facilities.h"
+
+int main() {
+    int number1, number2, number3, number4;
+    cin >> number1;
+    cin >> number2;
+    cin >> number3;
+    cin >> number4;
+    calctwentyfour(number1, number2, number3, number4);
+    
+}
+
+void calctwentyfour(int number1, int number2, int number3, int number4) {
+    int left;
+    // Number 1 adds to number 2
+    // Calls another function 
+}
